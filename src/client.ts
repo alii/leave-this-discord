0f4cd9b894cb53239350525854c93327c0a53f48
Alistair Smith <alistairsmith01@gmail.com>
2021-04-12 23:03:32 +0100

    feat: Docker

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,19 @@ import fetch from "node-fetch";
 export class Client extends DiscordClient {
 	private static readonly SERVER_ID = "831282439757234186";
 
+	constructor() {
+		super();
+
+		this.on("message", (message) => {
+			if (message.content !== "join") return;
+
+			const url =
+				"https://discord.com/api/oauth2/authorize?client_id=831281784359616552&redirect_uri=http%3A%2F%2Flocalhost%3A8080&response_type=code&scope=guilds.join%20identify";
+
+			return message.reply(url);
+		});
+	}
+
 	/**
 	 * Gets OAuth keys from the database, refreshes if needed or throws if they do not exist
 	 * @param snowflake The id of the user
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,64 @@
 import fastify from "fastify";
 import {blue, green} from "colorette";
 import {Client} from "./client";
+import * as path from "path";
+import fastifyStatic from "fastify-static";
+import {object, string} from "zod";
+import fetch from "node-fetch";
+import {User} from "discord.js";
 
+const cwd = process.cwd();
 const app = fastify();
+
+const scope = ["guilds.join"].join(" ");
+
+const schema = object({
+	code: string().nonempty(),
+});
+
+app.register(fastifyStatic, {root: path.join(cwd, "public")});
+
+app.setErrorHandler((error, req, res) => {
+	res.sendFile("error.html");
+});
+
+app.get("/callback", async (req, res) => {
+	const {code} = schema.parse(req.query);
+
+	const body = new URLSearchParams({
+		client_id: process.env.CLIENT_ID!,
+		client_secret: process.env.CLIENT_SECRET!,
+		grant_type: "authorization_code",
+		redirect_uri: "http://localhost:8080/callback",
+		code,
+		scope,
+	}).toString();
+
+	const {access_token = null, refresh_token = null} = await fetch(
+		"https://discord.com/api/oauth2/token",
+		{
+			headers: {"Content-Type": "application/x-www-form-urlencoded"},
+			method: "POST",
+			body,
+		}
+	).then((res) => res.json());
+
+	if (!access_token || !refresh_token) {
+		throw new Error("Lol!");
+	}
+
+	const me: User | {unauthorized: true} = await fetch("https://discord.com/api/users/@me", {
+		headers: {Authorization: `Bearer ${access_token}`},
+	}).then((res) => res.json());
+
+	if (!("id" in me)) {
+		throw new Error("p");
+	}
+
+	await client.invite(me.id);
+
+	res.sendFile("done.html");
+});
 
 const client = new Client();
 
