af781f2e93058329b59aa70848f66dd1c8230512
Alistair Smith <alistairsmith01@gmail.com>
2021-04-12 22:44:51 +0100

    move client boot to index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import fastify from "fastify";
-import {blue} from "colorette";
+import {blue, green} from "colorette";
+import {Client} from "./client";
 
 const app = fastify();
+
+const client = new Client();
+
+client.login(process.env.DISCORD_TOKEN).then(() => {
+	console.log(`${green("discord")} ready`);
+});
 
 app.listen(process.env.PORT || 8080).then(() => {
 	console.log(`${blue("http")} ready`);
